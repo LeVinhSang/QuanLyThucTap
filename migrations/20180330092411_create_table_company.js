fc8179a2d3c5a7318c65183c97ca644be0ade4e2
LeVinhSang <sanglv@sphinx-software.com>
2018-03-30 09:33:25 +0700

    update migrate

--- migrations/20180330092411_create_table_company.js
+++ migrations/20180330092411_create_table_company.js
@@ -1,15 +1,14 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('interns', function (table) {
+    return knex.schema.createTable('companies', function (table) {
         table.increments('id');
-        table.integer('code').notNull();
         table.string('name').notNull();
         table.integer('phone');
         table.string('email');
         table.string('address');
-    });
+    })
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('interns')
+    return knex.schema.dropTable('companies');
 };
